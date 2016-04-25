77af00c27f83427b5f3f2b99223f273a345ccc1e
raduan.santos <raduan.santos@softplan.com.br>
2016-04-25 08:15:09 -0300

    Initial methods

--- src/angular-data-storage.js
+++ src/angular-data-storage.js
@@ -1,11 +1,82 @@
 angular
-	.module('AngularDataStorage', [])
-	.provider('angularDataStorageService', function() {
+    .module('AngularDataStorage', [])
+    .provider('angularDataStorageService', function () {
 
-		this.$get = function() {
+        var parameters = {};
 
-			return {
+        this.$get = function () {
 
-			};
-		};
-	});
+            //options destroy: Boolean
+            var getFromStorage = function (key, options) {
+                if(angular.isUndefined(key)){
+                    throw 'The key cannot be undefined';
+                }
+                var value = angular.copy(parameters[key]);
+                console.log('The value:' + value);
+                if(angular.isDefined(options) &&  angular.isDefined(options.destroy) && options.destroy){
+                    parameters[key] = null;
+                }
+                return value;
+            };
+
+            //options asRef: Boolean cached: Boolean
+            var saveOnStorage = function (key, value, options) {
+                if(angular.isUndefined(key)){
+                    throw 'The key cannot be undefined';
+                }
+
+                if(angular.isDefined(options)) {
+                    if (angular.isDefined(options.cached) && options.cached) {
+                        console.log("Save asCached");
+                        return this;
+                    }
+                    if (angular.isDefined(options.asRef) && options.asRef) {
+                        console.log("Save asRef");
+                        return this;
+                    }
+                }
+                parameters[key] = angular.copy(value);
+                return this;
+            };
+
+            var deleteFromStorage = function (key) {
+
+            };
+
+            var getAllKeysFromStorage = function () {
+                //return parameters.keys();
+            };
+
+            var clearAllInStorage = function () {
+                //parameters.clear();
+            };
+
+            var eventChangeOnStorage = function (key, callback) {
+
+            };
+
+            var eventAddedOnStorage = function (key, callback) {
+
+            };
+
+            var eventDeletedFromStorage = function (key, callback) {
+
+            };
+
+            var addToCache = function (key) {
+
+            };
+
+            return {
+                get: getFromStorage,
+                save: saveOnStorage,
+                delete: deleteFromStorage,
+                keys: getAllKeysFromStorage,
+                clearAll: clearAllInStorage,
+                onChanged: eventChangeOnStorage,
+                onAdded: eventAddedOnStorage,
+                onDeleted: eventDeletedFromStorage,
+                toCache: addToCache
+            };
+        };
+    });
--- test/spec/angular-data-storage.spec.js
+++ test/spec/angular-data-storage.spec.js
@@ -1,5 +1,73 @@
 'use strict';
 
-describe('Angular Data Storage', function() {
-	beforeEach(module('AngularDataStorage'));
+describe('Angular Data Storage', function () {
+    var scope1, scope2;
+
+    function ctrl1($rootScope) {
+        scope1 = $rootScope.$new();
+    }
+
+    function ctrl2($rootScope) {
+        scope2 = $rootScope.$new();
+    }
+
+    beforeEach(module('AngularDataStorage'));
+    beforeEach(inject(function ($rootScope) {
+        ctrl1($rootScope);
+        ctrl2($rootScope);
+    }));
+
+    it('should return the value1 for object1', inject(function (angularDataStorageService) {
+        angularDataStorageService.save('object1', 'value1');
+
+        var obj1 = angularDataStorageService.get('object1');
+        expect(obj1).not.toBeNull();
+        expect(obj1).toBe('value1');
+    }));
+
+    it('should return the value1 for object1 destroying from storage', inject(function (angularDataStorageService) {
+        angularDataStorageService.save('object1', 'value1');
+
+        var obj1 = angularDataStorageService.get('object1', {'destroy': true})
+        expect(obj1).not.toBeNull();
+        expect(obj1).toBe('value1');
+        var obj2 = angularDataStorageService.get('object1');
+        expect(obj2).toBeNull();
+    }));
+
+    it('should scope1 set value for the provider', inject(function (angularDataStorageService) {
+        scope1.value = 'banana';
+        expect(scope1.value).toBe('banana');
+
+        angularDataStorageService.save('bnn', scope1.value);
+        var value = angularDataStorageService.get('bnn');
+        expect(value).toBe('banana');
+    }));
+
+    it('should scope1 set value for the provider and scope2 retrieve and set on itself ', inject(function (angularDataStorageService) {
+        scope1.value = 'banana';
+        expect(scope1.value).toBe('banana');
+        expect(scope2.value).toBeUndefined();
+
+        angularDataStorageService.save('bnn', scope1.value);
+        scope2.value = angularDataStorageService.get('bnn');
+        expect(scope1.value).toBe('banana');
+        expect(scope2.value).toBe('banana');
+    }));
+
+    it('should not data interfere with origin objects with default options', inject(function (angularDataStorageService) {
+        scope1.value = 'banana';
+        expect(scope1.value).toBe('banana');
+
+        angularDataStorageService.save('bnn', scope1.value);
+        scope2.value = angularDataStorageService.get('bnn');
+        expect(scope1.value).toBe('banana');
+        expect(scope2.value).toBe('banana');
+
+        scope1.value = 'apple';
+        expect(scope1.value).toBe('apple');
+        expect(scope2.value).toBe('banana');
+    }));
+
+
 });
