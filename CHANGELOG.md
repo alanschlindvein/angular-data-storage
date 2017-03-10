88af912a993aceb0fc245bb16e380b38f0c90d29
alan.araujo <alan.araujo@softplan.com.br>
2017-03-10 09:03:47 -0300

    added require/import support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 <a name="1.0.0"></a>
+# 0.1.0 Furious Bug Lady (2017-03-10)
+#### Now we can use with require/import.
+
+<a name="1.0.0"></a>
 # 0.1.0 Furious Bug Lady (2016-12-02)
 #### Simplified service API.
 #### Add length method.
--- src/angular-data-storage.js
+++ src/angular-data-storage.js
@@ -1,53 +1,74 @@
-angular
-   .module('AngularDataStorage', [])
-   .service('dataStorageService', dataStorageService);
+(function(factory) {
+  'use strict';
+  if (typeof exports === 'object') {
+    // Node/CommonJS
+    module.exports = factory(typeof angular !== 'undefined' ? angular : require('angular'));
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['angular'], factory);
+  } else {
+    // Browser globals
+    if (typeof angular === 'undefined') {
+      throw new Error('AngularJS framework needs to be included, see https://angularjs.org/');
+    }
+    factory(angular);
+  }
+}(function(angular) {
+  'use strict';
 
-   function dataStorageService() {
-      var self = this,
-         saver = {},
-         keyError = 'Key needs to be string.',
-         argumentsError = 'You need pass at least one key';
+  return angular
+    .module('AngularDataStorage', [])
+    .service('dataStorageService', dataStorageService);
 
-      function isInvalidKey(key) {
-         return typeof key !== 'string';
+  function dataStorageService() {
+    var self = this,
+      saver = {},
+      error = {
+        key: 'Key needs to be string.',
+        arguments: 'You need pass at least one key'
+      };
+
+    function isInvalidKey(key) {
+      return typeof key !== 'string';
+    }
+
+    this.fetch = function(key) {
+      if(isInvalidKey(key)) {
+        throw keyError;
       }
+      var value = saver[key];
+      return (typeof value !== 'undefined') ? value : null;
+    };
 
-      this.fetch = function(key) {
-         if(isInvalidKey(key)) {
-            throw keyError;
-         }
-         var value = saver[key];
-         return (typeof value !== 'undefined') ? value : null;
-      };
-
-      this.save = function(key, value) {
-         if(isInvalidKey(key)) {
-            throw keyError;
-         }
-         saver[key] = value;
-      };
-
-      this.delete = function() {
-         if(!arguments.length) {
-            throw argumentsError;
-         }
-         for(var i = 0; i < arguments.length; i++) {
-            if(isInvalidKey(arguments[i])) {
-               throw keyError;
-            }
-            delete saver[arguments[i]];
-         }
-      };
-
-      this.allKeys = function() {
-         return Object.keys(saver);
-      };
-
-      this.clearAll = function() {
-         saver = {};
-      };
-
-      this.length = function() {
-         return self.allKeys().length;
+    this.save = function(key, value) {
+      if(isInvalidKey(key)) {
+        throw keyError;
       }
-   }
+      saver[key] = value;
+    };
+
+    this.delete = function() {
+      if(!arguments.length) {
+        throw argumentsError;
+      }
+      for(var i = 0; i < arguments.length; i++) {
+        if(isInvalidKey(arguments[i])) {
+          throw keyError;
+        }
+        delete saver[arguments[i]];
+      }
+    };
+
+    this.allKeys = function() {
+      return Object.keys(saver);
+    };
+
+    this.clearAll = function() {
+      saver = {};
+    };
+
+    this.length = function() {
+      return self.allKeys().length;
+    }
+  }
+}));
